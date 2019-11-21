ba9704f8491b73bb30bd5c63aac95369d348c303
Sebastian <pl4yingnight@gmail.com>
2019-11-21 09:45:57 +0100

    [UPDATE] removed traces of dht11 base resource

--- DHTLib/DHTLib/dht.c
+++ DHTLib/DHTLib/dht.c
@@ -15,24 +15,24 @@
 #define DDR(x) (*(&x - 1))						/* address of input register of port x */
 #define PIN(x) (*(&x - 2))						/* address of data direction register of port x */
 
-#define dht11_DDR_in() DDR(DHT_PORT) &= ~(1<<DHT_PIN)
-#define dht11_DDR_out() DDR(DHT_PORT) |= (1<<DHT_PIN)
-#define dht11_PIN_low() DHT_PORT &= ~(1<<DHT_PIN)
-#define dht11_PIN_high() DHT_PORT |= (1<<DHT_PIN)
+#define dht_DDR_in() DDR(DHT_PORT) &= ~(1<<DHT_PIN)
+#define dht_DDR_out() DDR(DHT_PORT) |= (1<<DHT_PIN)
+#define dht_PIN_low() DHT_PORT &= ~(1<<DHT_PIN)
+#define dht_PIN_high() DHT_PORT |= (1<<DHT_PIN)
 
 uint8_t I_RH, D_RH, I_Temp, D_Temp, CheckSum;
 
 void Trigger()									/* micro controller send start pulse/request */
 {
-	dht11_PIN_high();
+	dht_PIN_high();
 	while(PIN(DHT_PORT) & (1<<DHT_PIN));		/* check if sensor has been reset */
 	
-	dht11_DDR_out();
-	dht11_PIN_low();							/* set to low pin */
+	dht_DDR_out();
+	dht_PIN_low();							/* set to low pin */
 	_delay_ms(20);								/* wait for 20ms */
-	dht11_PIN_high();							/* set to high pin */
+	dht_PIN_high();							/* set to high pin */
 
-	dht11_DDR_in();
+	dht_DDR_in();
 	while(PIN(DHT_PORT) & (1<<DHT_PIN));
 	while((PIN(DHT_PORT) & (1<<DHT_PIN))==0);
 	while(PIN(DHT_PORT) & (1<<DHT_PIN));
@@ -69,15 +69,15 @@ extern uint8_t UpdateData(){
 	return 0;
 }
 
-extern uint8_t dht11_rh_high(){
+extern uint8_t dht_rh_high(){
 	return I_RH;
 }
-extern uint8_t dht11_rh_low(){
+extern uint8_t dht_rh_low(){
 	return D_RH;
 }
-extern uint8_t dht11_temp_high(){
+extern uint8_t dht_temp_high(){
 	return I_Temp;
 }
-extern uint8_t dht11_temp_low(){
+extern uint8_t dht_temp_low(){
 	return D_Temp;
 }
--- DHTLib/DHTLib/dht.h
+++ DHTLib/DHTLib/dht.h
@@ -11,14 +11,14 @@
 #define DHT_H
 
 #define F_CPU			16000000	// 16 MHz
-#define DHT_PORT		PORTC		// port of the dht11 data pin
-#define DHT_PIN			3			// number of the dht11 data pin
+#define DHT_PORT		PORTC		// port of the DHT sensor data pin
+#define DHT_PIN			3			// number of the DHT sensor data pin
 
 extern uint8_t UpdateData();		// returns 1 if the checksum is wrong else 0 gets returned
 
-extern uint8_t dht11_rh_high();		// returns the humidity before the comma
-extern uint8_t dht11_rh_low();		// returns the humidity after the comma
-extern uint8_t dht11_temp_high();	// returns the temperature before the comma
-extern uint8_t dht11_temp_low();	// returns the temperature after the comma
+extern uint8_t dht_rh_high();		// returns the humidity before the comma
+extern uint8_t dht_rh_low();		// returns the humidity after the comma
+extern uint8_t dht_temp_high();		// returns the temperature before the comma
+extern uint8_t dht_temp_low();		// returns the temperature after the comma
 
-#endif /* DHT11_H_ */
+#endif /* DHT_H_ */
